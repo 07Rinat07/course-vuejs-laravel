b13dd8c19987c0c48406abc6b83bcce3b6ada9f3
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-23 00:57:33 +0500

    Deleting a person with axios__30_L_

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -8,23 +8,26 @@
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
                 <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="person in people">
-                <td>{{ person.name}}</td>
-                <td>{{ person.age}}</td>
-                <td>{{ person.job}}</td>
+                <td>
+                    <router-link :to="{ name: 'person.show', params: {id: person.id} }"> {{ person.name }}</router-link>
+                </td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
                 <td>
                     <router-link :to="{name: 'person.edit', params: {id: person.id}}">Edit</router-link>
                 </td>
+                <td>
+                    <a @click.prevent="deletePerson(person.id)" href="#" class="btn btn-outline-danger">Delete</a>
+                </td>
             </tr>
-
             </tbody>
         </table>
     </div>
-
-
 </template>
 
 <script>
@@ -40,13 +43,18 @@ export default {
     mounted() {
         this.getPeople()
     },
-
-
     methods: {
         getPeople() {
             axios.get('/api/people')
                 .then(res => {
                     this.people = res.data
+                })
+        },
+
+        deletePerson(id) {
+            axios.delete('/api/people/' + id)
+                .then(res => {
+                    this.getPeople()
                 })
         }
     }
